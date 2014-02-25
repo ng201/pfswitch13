5adb921a0fa86125f6c0f567b0ab543aa54fcca4
ng201 <gnemeth.mail@gmail.com>
2014-02-25 20:39:28 +0100

    Soe old files removed.

--- qpf_types.h
+++
@@ -1,9 +0,0 @@
-#ifndef _QPF_TYPES_H_
-#define _QPF_TYPES_H_
-
-#include "json_object.hh"
-
-typedef int QPF_IP_ADDRESS_TYPE;
-#define QPF_IP_ADDRESS_JSON vigil::json_object::JSONT_INTEGER
-
-#endif // _QPF_TYPES_H_
